416e1fc17a8f7cfe73619595995e89c5b89c7492
Eduardo Andrade <eduardoandrade@MacBook-Air-de-Eduardo.local>
2024-01-24 16:23:02 -0500

    Programa finalizado con documentacion lista

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> // Se definen las librerias 
 #include<stdlib.h>
-/*menu tres opciones; ingresar mostrar salir. guardar en archivo txt, de manera horizontal por producto*/
-struct producto{
+
+struct producto{ //Estructura establecida para los datos de los productos 
     char codigo[25];
     char nombreProd[50];
     char descrProd [50];
@@ -12,11 +12,11 @@ struct producto{
 
 int main()
 {
-    struct producto datos;
-    FILE *archivo;
+    struct producto datos; // Esto nos ayudara a posteriormente guardar los datos en la estructura
+    FILE *archivo; //Se declara un puntero
     int opc;
     do{
-        printf("Bienvenidos a Productos Andrade\n");
+        printf("Bienvenidos a Productos Andrade\n"); //Opciones del menu 
         printf("1. Ingresar datos del producto\n");
         printf("2. Mostrar los datos de los productos\n");
         printf("3. Salir\n");
@@ -24,10 +24,10 @@ int main()
         switch (opc)
         {
         case 1:
-        archivo = fopen("Productos.txt", "a");
-        if (archivo!= NULL)
+        archivo = fopen("Productos.txt", "w"); //Con la "w" creamos el archivo para poder escribir y leer al nuevo archivo de texto llamado Productos
+        if (archivo!= NULL) //Revisamos si el archivo realmente existe
         {
-            printf("Ingrese el codigo del producto\n");
+            printf("Ingrese el codigo del producto\n"); //Solicitamos los datos y con la variable datos junto . con la variable de la estructura se almacena el dato en dicha variable
             scanf("%s", datos.codigo);
             printf("Ingrese el nombre del producto\n");
             scanf("%s", datos.nombreProd);
@@ -37,32 +37,36 @@ int main()
             scanf("%d", &datos.cantidad);
             printf("Ingrese el precio del producto\n");
             scanf("%f", &datos.precioCompra);
+            fprintf(archivo, "%s %s %s %d %.2f\n", datos.codigo, datos.nombreProd, datos.descrProd, datos.cantidad, datos.precioCompra); //Con la funcion fprintf Imprimimos estos datos en el archivo de texto 
         }
-        fclose(archivo);
+        fclose(archivo); //Cerramos el archivo
         break;
         case 2:
-        
-        if ((archivo = fopen("Productos.txt", "r")) !=NULL) 
-        { 
-            while (!feof(archivo))
-            {
-                 printf(" Datos del producto: %s %s %s %d %f\n", datos.codigo, datos.nombreProd, datos.descrProd, datos.cantidad, datos.precioCompra);
-            }
-            fclose(archivo);
-        } else
+        archivo = fopen("Productos.txt", "r"); //Ahora abrimos el archivo pero con "r" solo para lectura 
+        if (archivo != NULL) //Verificamos si el archivo realmente existe
         {
-            printf("error con el archivo");
+            while (fscanf(archivo, "%s %s %s %d %f\n", datos.codigo, datos.nombreProd, datos.descrProd, &datos.cantidad, &datos.precioCompra) == 5 ) //Con el while realizamos una revision del archivo hasta que culmine, y se utiliza la funcion fscanf para comparar posteriormente si son 5 datos los cuales se estan leyendo, por eso se comprueba si es ==5
+            {
+                printf("Datos del producto: %s %s %s %d %.2f\n", datos.codigo, datos.nombreProd, datos.descrProd, datos.cantidad, datos.precioCompra); //Se imprimen los productos almacenado en el archivo Productos 
+            }
+            fclose(archivo); //Cerramos el archivo
+
+        }else
+        {
+            printf("Error al abrir el archivo.\n"); //Si el archivo no existe se envia un mensaje de error 
+
         }
-        
+        case 3:
+        printf("Saliendo...\n"); //Ultima opcion para salir del programa 
         break;
         default:
-            if (opc != 1 || opc !=2 || opc!= 3){
+            if (opc != 1 || opc !=2 || opc!= 3){ //Se verifica si no es una de las opciones dentro del menu para informar que la opcion no es correcta
                 printf("La opcion ingresada no es correcta\n");
             }
             break;
         }
 
-    } while (opc != 3);
+    } while (opc != 3); //Se repite el menu hasta que el usuario quiera salir del programa 
     
     return 0;
 }
