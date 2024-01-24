9056e2622d30f37533364fc229ced35ce1d8df54
Eduardo Andrade <eduardoandrade@MacBook-Air-de-Eduardo.local>
2024-01-24 15:23:30 -0500

    Funcion para ingresar productos creada

--- main.c
+++ main.c
@@ -8,6 +8,29 @@ struct producto{
     float precioCompra;
     
 };
+
+void ingresarProducto() {
+    struct producto datos;
+    FILE *archivo;
+
+    archivo = fopen("Productos.txt", "w");
+    if ((archivo=fopen("Productos.txt", "w"))!= NULL)
+    {
+        printf("Ingrese el codigo del producto\n");
+        scanf("%s", datos.codigo);
+        printf("Ingrese el nombre del producto\n");
+        scanf("%s", datos.nombreProd);
+        printf("Ingrese la descripcion del producto\n");
+        scanf("%s", datos.descrProd);
+        printf("Ingrese la cantidad de productos\n");
+        scanf("%d", &datos.cantidad);
+        printf("Ingrese el precio del producto\n");
+        scanf("%f", &datos.precioCompra);
+    }
+    fclose(archivo);
+    
+}
+
 int main(int argc, char const *argv[])
 {
     int opc;
@@ -18,11 +41,18 @@ int main(int argc, char const *argv[])
         printf("2. Mostrar los datos de los productos\n");
         printf("3. Salir\n");
         scanf("%d", &opc);
-
+        switch (opc)
+        {
+        case 1:
+            ingresarProducto;
+            break;
+        
+        default:
+            break;
+        }
 
     } while (opc != 3);
     
     return 0;
 }
-
 
